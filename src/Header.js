47f6cfb10d37b0b575abcc8f39972c0eab7f6130
Karan Monga <karanm777@gmail.com>
2021-08-15 15:01:52 -0400

    User Auth started 3

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,18 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+      console.log("Yooo " + user);
+      console.log("ding");
+    }
+  };
 
   return (
     <div className="header">
@@ -24,14 +33,17 @@ function Header() {
           <SearchIcon className="header_searchIcon"></SearchIcon>
           {/* Logo */}
         </div>
-        
+
         <div className="header_nav">
-        <Link to="/login">
-          <div className="header_link">
-            <span className="header_link_line1">Hello Guest</span>
-           <span className="header_link_line2">Sign In</span>
-          </div>
+          <Link onClick={handleAuthentication} to={!user && "/login"}>
+            <div className="header_link">
+              <span className="header_link_line1">Hello Guest</span>
+              <span className="header_link_line2">
+                {user ? "Sign out" : "Sign in"}
+              </span>
+            </div>
           </Link>
+
           <div className="header_link">
             <span className="header_link_line1">Return</span>
             <span className="header_link_line2">& Orders</span>
@@ -66,11 +78,12 @@ function Header() {
           </Link>
         </div>
         <div className="row2-right">
-        <Link to="/checkout">
+          <Link to="/checkout">
             <div className="header_link-cart">
               <span className="spaced-link">Your orders</span>
             </div>
-          </Link><Link to="/checkout">
+          </Link>
+          <Link to="/checkout">
             <div className="header_link-cart">
               <span className="spaced-link">Help</span>
             </div>
--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import Gallery from "react-amazon-gallery";
 
-function ProductPage() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+let images = [
+  "https://m.media-amazon.com/images/I/71zny7BTRlL._AC_SL1500_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/81ky0potEEL._AC_SL1500_.jpg",
+  "https://m.media-amazon.com/images/I/71b9icxgIgL._AC_SL1000_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/61QECaX%2BDqL._AC_SL1500_.jpg",
+];
 
-export default ProductPage
+const ProductPage = () => {
+  return (
+    <div>
+      <Gallery images={images} />
+    </div>
+  );
+};
+
+export default ProductPage;
